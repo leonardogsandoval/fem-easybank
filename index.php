dc22ac6b3cf19f226df2ee87b347db06232ec3b2
Leonardo Gomez <lgomezs80@hotmail.com>
2021-02-16 15:21:50 -0500

    language support

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php include_once("home.html"); ?>
+<?php 
+
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+switch ($lang){
+    case "es":
+        include("home_es.php");
+        break;
+    case "it":
+        include("index_it.php");
+        break;
+    case "en":
+        include("home.php");
+        break;
+    default:
+        // Incluye EN en cualquier idioma que no sean los anteriores:
+        include("index_en.php");
+        break;
+}
+
+ ?>
