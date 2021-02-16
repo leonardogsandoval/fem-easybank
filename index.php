c8bc38de07a28fe8c708c4a3663b267fb12e4589
Leonardo Gomez <lgomezs80@hotmail.com>
2021-02-16 15:24:15 -0500

    language support

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 switch ($lang){
     case "es":
-        include("home_es.php");
+        include("home_es.html");
         break;
     case "it":
         include("index_it.php");
         break;
     case "en":
-        include("home.php");
+        include("home.html");
         break;
     default:
         // Incluye EN en cualquier idioma que no sean los anteriores:
